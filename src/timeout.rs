09bfb0cb2a7f416514a84d832d26959b20bd31fd
amrbashir <48618675+amrbashir@users.noreply.github.com>
2021-10-04 18:35:40 +0200

    chore: update licensing

--- src/timeout.rs
+++ src/timeout.rs
@@ -1,5 +1,6 @@
 // Copyright 2019-2021 Tauri Programme within The Commons Conservancy
 // SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
 
 /// Describes the timeout of a notification
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
